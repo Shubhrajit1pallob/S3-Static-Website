619aedf08a8a13224c6698de7fb49799d0579bc3
Shubhrajit1pallob <spallob@asu.edu>
2025-05-29 10:17:17 -0700

    Created the Readme file and the S3 bucket. Also created policies that denies any public access to the bucket and can only be read from it.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# S3 Static Website (Terraform)
+
+1. Create the s3 bucket.
+2. Configure the S3 bucket so that it has public access to it. This will help us get access to the html files that are there inside the bucket.
+3. Create a S3 bucket policy that will not let anyone write to it but they can read it.
--- s3.tf
+++ s3.tf
@@ -5,3 +5,31 @@ resource "random_id" "bucket_name" {
 resource "aws_s3_bucket" "static_website_bucket" {
   bucket = "my-static-website-bucket-${random_id.bucket_name.hex}"
 }
+
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_website_public_read" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  # This is the policy that allows public read access to the objects in the S3 bucket.
+  # The function jsonencode is used to convert the policy into a JSON string.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
+      }
+    ]
+  })
+}
